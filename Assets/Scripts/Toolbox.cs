adfe8e058ba2d1ed1c180c8f2651e365f3a80aee
Shahid Khaliq <dodupersonal@gmail.com>
2016-04-23 14:53:31 +0500

    Added model syncing. Need to test and check frame rate.

--- Assets/Scripts/Toolbox.cs
+++ Assets/Scripts/Toolbox.cs
@@ -2,30 +2,29 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class Toolbox : MonoBehaviour {
+public class Toolbox : MonoBehaviour
+{
 
-	public GameObject Cube;
-	public GameObject Sphere;
-	public GameObject Cylinder;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (SixenseInput.Controllers [0].GetButtonDown (SixenseButtons.ONE))
-			Instantiate (Cube,transform.position,Quaternion.identity);
-		if (SixenseInput.Controllers [0].GetButtonDown (SixenseButtons.TWO))
-			Instantiate (Sphere,transform.position,Quaternion.identity);
-		if (SixenseInput.Controllers [0].GetButtonDown (SixenseButtons.THREE))
-			Instantiate (Cylinder,transform.position,Quaternion.identity);
-		if (SixenseInput.Controllers [1].GetButtonDown (SixenseButtons.ONE))
-			Instantiate ((Object)Grabber.current,transform.position,Quaternion.identity);
-		if (SixenseInput.Controllers [1].GetButtonDown (SixenseButtons.TWO))
-			Destroy ((Object)Grabber.current);
-		if(SixenseInput.Controllers [0].GetButtonDown (SixenseButtons.START))
-			SceneManager.LoadScene (SceneManager.GetActiveScene().name);		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SixenseInput.Controllers[0].GetButtonDown(SixenseButtons.ONE))
+            PhotonNetwork.Instantiate("SplitableCube", transform.position, Quaternion.identity, 0);
+        if (SixenseInput.Controllers[0].GetButtonDown(SixenseButtons.TWO))
+            PhotonNetwork.Instantiate("SplitableSphere", transform.position, Quaternion.identity, 0);
+        if (SixenseInput.Controllers[0].GetButtonDown(SixenseButtons.THREE))
+            PhotonNetwork.Instantiate("SplitableCylinder", transform.position, Quaternion.identity, 0);
+        if (SixenseInput.Controllers[1].GetButtonDown(SixenseButtons.ONE))
+            Instantiate((Object)Grabber.current, transform.position, Quaternion.identity);
+        if (SixenseInput.Controllers[1].GetButtonDown(SixenseButtons.TWO) && (Object)Grabber.current != null)
+            Destroy((Object)Grabber.current);
+        if (SixenseInput.Controllers[0].GetButtonDown(SixenseButtons.START))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
